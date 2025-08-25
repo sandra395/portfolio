45890866e3e7cb3e7b548e8dc8dc9128010a3340
Sandra Barbosa <sandrabarbosa@Sandras-MacBook-Pro.local>
2025-08-25 13:53:52 +0100

    set up hosting

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // Load the app setup from app.js
 const app = require("./app");
 
+// Destructure PORT from environment variables, defaulting to 9090 if not set
+const{PORT = 9090} = process.env;
+
 // Start the server
-app.listen(9090, () => {
+app.listen(PORT,()=> {
   console.log("listening...");
 });
